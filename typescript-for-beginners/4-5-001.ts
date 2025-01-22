c4724ee6414d25589fa51d012f416cfbecfe187b
space-cap <hong.git@yahoo.com>
2025-01-22 12:08:49 +0900

    4-5

--- typescript-for-beginners/4-5-001.ts
+++ typescript-for-beginners/4-5-001.ts
@@ -2,3 +2,19 @@ interface SStorage<T> {
     [key:string]: T;
 }
 
+class LocalStorage<T> {
+    private storage: SStorage<T>;
+
+    constructor() {
+        this.storage = {};
+    }
+
+    setItem(key: string, value: T) {
+        this.storage[key] = value;
+    }
+
+    getItem(key: string): T {
+        return this.storage[key];
+    }
+}
+
