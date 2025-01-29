b7f13dc74f421df84133af41673e0dba859820cf
COCO <hong.git@yahoo.com>
2025-01-29 22:27:27 +0900

    ts

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,4 +1,27 @@
-console.log('Hello, world! hi');
+import crypto from 'crypto';
+
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape {
+    public hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+
+    constructor(hash: string, prevHash: string, height: number, data: string) {
+        this.hash = Block.calulateHash(prevHash, height, data);
+    }
+
+    static calulateHash = (prevHash: string, height: number, data: string): string => {
+        const crypto = require('crypto');
+    }
+}
 
 
 
